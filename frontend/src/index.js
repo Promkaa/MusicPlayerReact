7c86ebb8be68ce8f8532c8ee0bdbfbba290e0f2e
Promkaa <165302870+Promkaa@users.noreply.github.com>
2026-05-15 05:34:02 +0700

    Add files via upload

---
+++ frontend/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+
+const originalError = console.error;
+console.error = (...args) => {
+    if (args[0]?.includes('Unchecked runtime.lastError')) {
+        return;
+    }
+    originalError.apply(console, args);
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
